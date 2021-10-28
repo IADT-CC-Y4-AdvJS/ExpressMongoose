aa3fb32cda7f735dae313081d396b1d66fc8e58c
John Dempsey <john.dempsey@iadt.ie>
2021-10-28 11:44:43 +0100

    Implement register feature

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -23,47 +23,42 @@ const loginUser = async (email, jwt) => {
 export default class UsersController {
     static async register(req, res) {
         try {
-            // const userFromBody = req.body;
-            // let errors = {};
-            // if (userFromBody && userFromBody.password.length < 8) {
-            //     errors.password = "Your password must be at least 8 characters.";
-            // }
-            // if (userFromBody && userFromBody.name.length < 3) {
-            //     errors.name = "You must specify a name of at least 3 characters.";
-            // }
+            const userFromBody = req.body;
+            let errors = {};
+            if (userFromBody && userFromBody.password.length < 8) {
+                errors.password = "Your password must be at least 8 characters.";
+            }
+            if (userFromBody && userFromBody.name.length < 3) {
+                errors.name = "You must specify a name of at least 3 characters.";
+            }
 
-            // if (Object.keys(errors).length > 0) {
-            //     res.status(400).json(errors);
-            //     return;
-            // }
+            if (Object.keys(errors).length > 0) {
+                res.status(400).json(errors);
+                return;
+            }
 
-            // const userInfo = {
-            //     ...userFromBody,
-            //     password: await hashPassword(userFromBody.password),
-            // };
+            const userInfo = {
+                ...userFromBody,
+                password: await hashPassword(userFromBody.password),
+            };
 
-            // const insertResult = await UsersDAO.addUser(userInfo);
-            // if (!insertResult.success) {
-            //     errors.email = insertResult.error;
-            // }
-            // const userFromDB = await UsersDAO.getUser(userFromBody.email);
-            // if (!userFromDB) {
-            //     errors.general = "Internal error, please try again later";
-            // }
+            const user = new User(userInfo);
+            await user.save();
+            
+            const userFromDB = await User.findOne({"email": user.email}).exec();
+            if (!userFromDB) {
+                errors.general = "Internal error, please try again later";
+            }
 
-            // if (Object.keys(errors).length > 0) {
-            //     res.status(400).json(errors);
-            //     return;
-            // }
+            if (Object.keys(errors).length > 0) {
+                res.status(400).json(errors);
+                return;
+            }
 
-            // const user = new User(userFromDB);
-
-            // res.json({
-            //     auth_token: user.encoded(),
-            //     info: user.toJson(),
-            // });
-
-            res.status(500).json({ "error" : "Not yet implemented" });
+            res.json({
+                auth_token: user.encoded(),
+                info: user.toJson(),
+            });
         } 
         catch (e) {
             res.status(500).json({ error: e });
